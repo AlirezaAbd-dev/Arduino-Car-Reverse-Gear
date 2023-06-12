d8caaf145e3bac35d168f4a30b46bf6861c5f418
0AliReza0 <alireza.abedi9310@gmail.com>
2023-06-12 20:02:37 +0330

    project created...

---
+++ car_reverse_gear.ino
@@ -0,0 +1,36 @@
+#include "SRF05.h"
+
+const int trigger = 7;
+const int echo = 6;
+
+SRF05 SRF(trigger, echo);
+
+int buzzer = 12;
+int limit = 40;
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(115200);
+
+  SRF.setCorrectionFactor(1.035);
+}
+
+void loop() {
+  float distance = SRF.getCentimeter();
+  Serial.print(String(distance));
+
+  if (distance < limit && distance > limit / 2) {
+    tone(buzzer, 330, 500);
+  }
+
+  if (distance < limit / 2 && distance > limit / 4) {
+    tone(buzzer, 330, 700);
+  }
+
+  if (distance < limit / 4) {
+    tone(buzzer, 330, 900);
+  }
+
+  delay(800);
+  noTone(buzzer);
+}
